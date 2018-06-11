31ee0fee226c36c21a92d38fba7146f3b3cf147c
Pieter PREEZ <ppreez@e4r14p6.wethinkcode.co.za>
2018-06-11 14:20:01 +0200

    Making progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ppreez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 11:25:43 by ppreez            #+#    #+#             */
-/*   Updated: 2018/06/09 12:21:08 by ppreez           ###   ########.fr       */
+/*   Updated: 2018/06/11 14:18:05 by ppreez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,18 @@
 
 void	assign(const int fd, char **line, t_list *node, char *buffer)
 {
-	size_t	i;
+	int	i;
+	char *str;
 
 	i = 0;
-	if (node->content == NULL)
-	{
-		printf("If assign\n");
-		node->content = (char *)ft_memalloc(sizeof(char *) * ft_linelen(buffer, BUFF_SIZE));
-		while (i < ft_linelen(buffer, BUFF_SIZE))
-		{
-			node->content = ft_strjoin((char *)node->content, buffer);
-			i++;
-		}
-	}
-	else
-	{
-		printf("Else assign\n");
-		node->content = ft_strjoin((char *)node->content + ft_linelen(buffer, BUFF_SIZE), buffer);
-	}
+	str = (char *)node->content;
+	while (str[i] != '\n')
+		i++;
+
+	*line = ft_strnew(i);
+	ft_strncpy(*line, node->content, i);
+	printf("*line: %s\n", *line);
+	node->content = ft_strdup(node->content + (i + 1));
 }
 
 
@@ -41,25 +35,20 @@ void	assign(const int fd, char **line, t_list *node, char *buffer)
 void	readlist(const int fd, char **line, t_list **node, char *buffer)
 {
 	printf("Node create\n");
-	read(fd, buffer, BUFF_SIZE);
+	if ((*node))
+		read(fd, buffer, BUFF_SIZE);
 	if (!(*node))
 	{
 		printf("ft_lstnew call\n");
+		read(fd, buffer, BUFF_SIZE);
 		(*node) = ft_lstnew(buffer, BUFF_SIZE);
-		//return ;
+		(*node)->content_size = fd;
 	}
-	printf("Node traverse\n");
-//	printf("Node->content print: %s\n", (*node)->content);
-	while ((*node)->content_size != fd && (*node)->next != NULL)
-	{
-		printf("traversal in loop\n");
-		(*node) = (*node)->next;
-	}
-	(*node)->content_size = fd;
+	printf("fd: %d\n", fd);
+	printf("Node content: \n%s\n", (*node)->content);
 	if ((*node)->content_size == fd)
 	{
 		printf("Assign call\n");
-		*line = ft_strdup((char *)(*node)->content + (ft_linelen((*node)->content, BUFF_SIZE)));
 		assign(fd, line, (*node), buffer);
 	}
 	//else if (node->content_size != fd && node->next == NULL)
@@ -70,7 +59,6 @@ void	readlist(const int fd, char **line, t_list **node, char *buffer)
 		
 int	get_next_line(const int fd, char **line)
 {
-	int				read_val;
 	char			buffer[BUFF_SIZE + 1];
 	static t_list	*node = NULL;
 
